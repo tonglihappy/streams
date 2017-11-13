9f372734fc9c959ba1f8ac8e36261ded6edd3830
root <root@vbu-offline-10-100-51-91.(none)>
2017-11-13 14:55:13 +0800

    update

--- include/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UTILS_H
-#define _UTILS_H
-
-void *grow_array(void *array, int elem_size, int *size, int new_size);
-
-#define GROW_ARRAY(array, nb_elems)\
-	array = grow_array(array, sizeof(*array), &nb_elems, nb_elems + 1)
-
-#endif
